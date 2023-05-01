3eeca514c383d526b0dd3373e25173d580afea0f
sahassan012 <sahassan012@gmail.com>
2023-04-30 17:32:59 -0700

    add app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from mongodb_utils import get_professor_by_name, get_all_professors, get_num_cited_publications_by_year
+from neo4j_utils import get_top_10_keywords_by_university
+
+#print(get_professor_by_name('Li,Qun'))
+#print(get_top_3_most_cited_publications())
+#print(get_all_professors())
+#print(get_num_cited_publications_by_year())
+
+#get_top_10_keywords_by_university('University of illinois at Urbana Champaign')
