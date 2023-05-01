d9198f5f0ec0760b6bfd79780af6f5731fde30b7
sahassan012 <sahassan012@gmail.com>
2023-04-30 18:00:45 -0700

    include add and remove methods for faculty members

--- mysql_utils.py
+++ mysql_utils.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import Error
 
 db = mysql.connector.connect(
     host="localhost",
@@ -9,3 +10,25 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
+def add_faculty(faculty):
+    query = """
+    INSERT INTO faculty_all_attributes
+    (id, name, position, researchInterest, email, phone, photoUrl, keywords, publications, `affiliation.id`, `affiliation.name`, `affiliation.photoUrl`)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    try:
+        cursor.execute(query, faculty)
+        db.commit()
+        print(f"Faculty member added successfully.")
+    except Error as e:
+        print(f"There was an error adding faculty member: {e}")
+
+def remove_faculty(name):
+    query = "DELETE FROM faculty_all_attributes WHERE name = %s"
+
+    try:
+        cursor.execute(query, (name,))
+        db.commit()
+        print(f"Faculty member deleted successfully.")
+    except Error as e:
+        print(f"There was an error deleting faculty member: {e}")
